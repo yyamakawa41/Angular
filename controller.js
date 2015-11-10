61cdbf1f85587b77824a1ef8dab85aa3cbf2a04e
yyamakawa41 <yyamakawa@gmail.com>
2015-11-10 10:36:03 -0500

    Array
    created an array

---
+++ controller.js
@@ -0,0 +1,29 @@
+angular.module('myApp', []).controller('myController', function($scope){
+	$scope.items = [
+		{
+			name: 'Freddy',
+			desc: 'Made a cool football game'
+		},
+		{
+			name: 'Chance',
+			desc: 'Bootsrap Guy'
+		},
+		{
+			name: 'Will',
+			desc: 'Wig guy'
+		},
+		{
+			name: 'Stephen',
+			desc: 'Awesome theme'
+		},
+		{
+			name: 'Griffin',
+			desc: 'DOTA2 addict'
+		},
+		{
+			name: 'Oliver',
+			desc: 'Eats 8 times/day'
+		},
+
+	];
+});
